00f598532cea5b4c516da9db286cd5793b5a51ac
20WH1A0556 <93443586+MythiliRevu@users.noreply.github.com>
2022-12-07 15:42:16 +0530

    Update colorOfSquare.cpp

--- decision making/colorOfSquare.cpp
+++ decision making/colorOfSquare.cpp
@@ -5,7 +5,7 @@ int main(){
 	string c;
 	cout<<"Enter character and integer: ";
 	cin>>c>>n;
-	if((c=="a"||c=="c"||c=="e"||c=="g") && (n==2||n==4||n==6||n==8)){
+	if(((c=="a"||c=="c"||c=="e"||c=="g")&&(n==2||n==4||n==6||n==8))||((c=="b"||c=="d"||c=="f"||c=="h")&&(n==1||n==3||n==5||n==7))){
 		cout<<"white";
 	}
 	else{
