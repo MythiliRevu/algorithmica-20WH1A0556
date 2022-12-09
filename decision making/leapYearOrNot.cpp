4a0bf836f290062db88026122ba108df66b1486e
20WH1A0556 <93443586+MythiliRevu@users.noreply.github.com>
2022-12-09 14:48:27 +0530

    Update leapYearOrNot.cpp

--- decision making/leapYearOrNot.cpp
+++ decision making/leapYearOrNot.cpp
@@ -4,8 +4,11 @@ int main(){
 	int n;
 	cout<<"Enter a year: ";
 	cin>>n;
-	if(n%400==0 || n%4==0){
+	if(n%400==0 && n%4==0){
 		cout<<"it is a leap year";
+	}
+	else if(n%100==0){
+		cout<<"it is not a leap year";
 	}
 	else{
 		cout<<"it is not a leap year";
