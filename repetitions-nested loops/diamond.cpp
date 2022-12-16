9eecd44c9981856b94d0a88cd09b6f9641de6031
20WH1A0556 <20wh1a0556@bvrithyderabad.edu.in>
2022-12-16 14:43:04 +0530

    partial submission

---
+++ repetitions-nested loops/diamond.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,i,j,k;
+	cout<<"Enter value: ";
+	cin>>n;
+    for(i=1; i<=n; i++){
+        for(j=n; j>i; j--){
+            cout<<" ";
+        }
+        for(k=0; k<i*2-1; k++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(i=1; i<=n-1; i++){
+        for(j=0; j<i; j++){
+            cout << " ";
+        }
+        for(k=(n-i)*2-1; k>0; k--){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
